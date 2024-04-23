894fbbf84c6df30b53cbd2e2ba66b7b509731d24
myrmanshelby <myrmanshelby@gmail.com>
2024-04-22 23:38:43 -0700

    Made operator buttons functional and appear on upper display

--- script.js
+++ script.js
@@ -12,31 +12,57 @@ let num1Input = "";
 let num2Input = "";
 let operatorInput = "";
 
-/*
-Getting function and display values to work
+// BUG: DOESN'T ALLOW USER TO INPUT MULTIPLE DIGIT NUMBERS
+// BUG: 8 * 9 = / in upper display because of operatorBtns event listener
+//    solution: need to create a function to populate the upper display, which either adds content or
+//    replaces based on existence of num1Input, num2Input, and operatorInput
+//    then replace this with what's in numberBtns and operatorBtns
 
-PSEUDOCODE
-
-for each number button
-add event listener that populates the display 
-and saves the number as the display number
-
-*/
+const upperDisplay = document.querySelector(".upper-display");
 
 // Makes number buttons work
 
 const numberBtns = document.querySelectorAll(".numbers");
 numberBtns.forEach((button) => {
   button.addEventListener("click", () => {
-    //update upper display
     populateLowerDisplay(button.id[1]);
 
-    if (num1Input == "") {
+    if (num1Input === "") {
       num1Input = parseFloat(button.id[1]);
+      upperDisplay.textContent = num1Input;
     } else {
       num2Input = parseFloat(button.id[1]);
+      upperDisplay.textContent += " " + num2Input;
     }
   });
+});
+
+// Makes operator buttons work
+const operatorBtns = document.querySelectorAll(".operator");
+operatorBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (operatorInput === "") {
+      operatorInput = button.id;
+      upperDisplay.textContent += " " + getOperatorString(button.id);
+    } else {
+      num1Input = operate(operatorInput, num1Input, num2Input);
+      num2Input = "";
+      operatorInput = button.id;
+      upperDisplay.textContent =
+        num1Input + " " + getOperatorString(operatorInput);
+      populateLowerDisplay(num1Input);
+    }
+  });
+});
+
+const equalsBtn = document.getElementById("equals");
+equalsBtn.addEventListener("click", () => {
+  num1Input = operate(operatorInput, num1Input, num2Input);
+  num2Input = "";
+  operatorInput = "";
+
+  upperDisplay.textContent += " =";
+  populateLowerDisplay(num1Input);
 });
 
 // Functions to populate the display
@@ -68,13 +94,26 @@ function divide(a, b) {
 // or second operator is called
 function operate(operator, num1, num2) {
   switch (operator) {
-    case "+":
+    case "plus":
       return add(num1, num2);
-    case "-":
+    case "minus":
       return subtract(num1, num2);
-    case "*":
+    case "multiply":
       return multiply(num1, num2);
-    case "/":
+    case "divide":
       return divide(num1, num2);
+  }
+}
+
+function getOperatorString(operator) {
+  switch (operator) {
+    case "plus":
+      return "+";
+    case "minus":
+      return "-";
+    case "multiply":
+      return "*";
+    case "divide":
+      return "/";
   }
 }
