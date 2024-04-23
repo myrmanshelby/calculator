9c6db9838886e27237294c568a451a6bb20a5cd3
myrmanshelby <myrmanshelby@gmail.com>
2024-04-22 22:52:50 -0700

    The number buttons now populate on the display

--- script.js
+++ script.js
@@ -8,22 +8,36 @@ The max length of a number with a decimal is 15 digits
 The max length of a number without a decimal is 16 digits
 */
 
-let num1Input;
-let num2Input;
-let operatorInput;
+let num1Input = "";
+let num2Input = "";
+let operatorInput = "";
 
-function operate(operator, num1, num2) {
-  switch (operator) {
-    case "+":
-      return add(num1, num2);
-    case "-":
-      return subtract(num1, num2);
-    case "*":
-      return multiply(num1, num2);
-    case "/":
-      return divide(num1, num2);
-  }
-}
+/*
+Getting function and display values to work
+
+PSEUDOCODE
+
+for each number button
+add event listener that populates the display 
+and saves the number as the display number
+
+*/
+
+// Makes number buttons work
+
+const numberBtns = document.querySelectorAll(".numbers");
+numberBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    //update upper display
+    populateLowerDisplay(button.id[1]);
+
+    if (num1Input == "") {
+      num1Input = parseFloat(button.id[1]);
+    } else {
+      num2Input = parseFloat(button.id[1]);
+    }
+  });
+});
 
 // Functions to populate the display
 
@@ -48,4 +62,19 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+// Allows use of mathematical operators when equal function
+// or second operator is called
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
 }
