5138a135979893c3f087726dea8974212d7efe59
myrmanshelby <myrmanshelby@gmail.com>
2024-04-26 16:39:35 -0700

    Fixed bugs to make display and result work correctly

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-/* TO DO
-- Make the functions to populate the display
-- Connect the buttons/make the calculator work
-*/
-
 /* Notes on rounding:
 The max length of a number with a decimal is 15 digits
 The max length of a number without a decimal is 16 digits
@@ -11,44 +6,22 @@ The max length of a number without a decimal is 16 digits
 let num1Input = "";
 let num2Input = "";
 let operatorInput = "";
+let result = "";
 let enteringNum1 = true;
 
-// BUG: DOESN'T ALLOW USER TO INPUT MULTIPLE DIGIT NUMBERS
-//      solution use a boolean that tracks if the lastPressed was a number or operator
-// BUG: 8 * 9 = / in upper display because of operatorBtns event listener
-//    solution: need to create a function to populate the upper display, which either adds content or
-//    replaces based on existence of num1Input, num2Input, and operatorInput
-//    then replace this with what's in numberBtns and operatorBtns
-
-const upperDisplay = document.querySelector(".upper-display");
-
 // Makes number buttons work
-/*
-const numberBtns = document.querySelectorAll(".numbers");
-numberBtns.forEach((button) => {
-  button.addEventListener("click", () => {
-    populateLowerDisplay(button.id[1]);
-
-    if (num1Input === "") {
-      num1Input = parseFloat(button.id[1]);
-      upperDisplay.textContent = num1Input;
-    } else {
-      num2Input = parseFloat(button.id[1]);
-      upperDisplay.textContent += " " + num2Input;
-    }
-  });
-});*/
-
 const numberBtns = document.querySelectorAll(".numbers");
 numberBtns.forEach((button) => {
   button.addEventListener("click", () => {
     if (enteringNum1) {
       num1Input += button.id[1];
-      upperDisplay.textContent += button.id[1];
+      //upperDisplay.textContent += button.id[1];
+      populateUpperDisplay();
       populateLowerDisplay(num1Input);
     } else {
       num2Input += button.id[1];
-      upperDisplay.textContent += button.id[1];
+      //upperDisplay.textContent += button.id[1];
+      populateUpperDisplay();
       populateLowerDisplay(num2Input);
     }
   });
@@ -58,29 +31,37 @@ numberBtns.forEach((button) => {
 const operatorBtns = document.querySelectorAll(".operator");
 operatorBtns.forEach((button) => {
   button.addEventListener("click", () => {
+    if (num1Input === "") {
+      num1Input = result;
+    }
     if (operatorInput === "") {
       operatorInput = button.id;
-      upperDisplay.textContent += " " + getOperatorString(button.id) + " ";
+      //upperDisplay.textContent += " " + getOperatorString(button.id) + " ";
       enteringNum1 = false;
     } else {
       num1Input = operate(operatorInput, num1Input, num2Input);
       num2Input = "";
       operatorInput = button.id;
-      upperDisplay.textContent =
-        num1Input + " " + getOperatorString(operatorInput);
+      //upperDisplay.textContent =
+      //  num1Input + " " + getOperatorString(operatorInput) + " ";
       populateLowerDisplay(num1Input);
+      enteringNum1 = false;
     }
+
+    populateUpperDisplay();
   });
 });
 
 const equalsBtn = document.getElementById("equals");
 equalsBtn.addEventListener("click", () => {
-  num1Input = operate(operatorInput, num1Input, num2Input);
+  result = operate(operatorInput, num1Input, num2Input);
+  num1Input = "";
   num2Input = "";
   operatorInput = "";
-
-  upperDisplay.textContent = num1Input;
-  populateLowerDisplay(num1Input);
+  const upperDisplay = document.querySelector(".upper-display");
+  upperDisplay.textContent = result;
+  enteringNum1 = true;
+  populateLowerDisplay(result);
 });
 
 // Functions to populate the display
@@ -90,7 +71,18 @@ function populateLowerDisplay(outputNumber) {
   lowerDisplay.textContent = outputNumber;
 }
 
-function populateUpperDisplay(text) {}
+function populateUpperDisplay() {
+  const upperDisplay = document.querySelector(".upper-display");
+  if (operatorInput === "") {
+    upperDisplay.textContent = num1Input;
+  } else if (num2Input === "") {
+    upperDisplay.textContent =
+      num1Input + " " + getOperatorString(operatorInput);
+  } else {
+    upperDisplay.textContent =
+      num1Input + " " + getOperatorString(operatorInput) + " " + num2Input;
+  }
+}
 
 // Below are the mathematical functions used by the calculator.
 // They are called by the operate function to run the calculator
